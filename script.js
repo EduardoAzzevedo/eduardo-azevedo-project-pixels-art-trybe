517830d16061c156b8fec7248f13fdb9e1aeeec4
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-21 10:30:17 -0300

    ajustes no HTML, CSS e JS.

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-// 4 - Cria quadro de pixel, pixels dentro do quadro, classes e id.
-//posso tentar usar uma function que será usada dentro de outra function para definir
-//quantos quadrados serão criados(quantos divs serão criados com createElement)
-let color = " ";
-const button = document.getElementById('clear-button');
+let color = 'rgb(0, 0, 0)';
+const button = document.getElementById('clear-board');
 const palette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
 
-for (let index = 0; index < 5; index += 1) {
-  const pixelParent = document.createElement('section');
-  pixelBoard.appendChild(pixelParent);
-  for (let index2 = 0; index2 < 5; index2 += 1) {
-  
-    window.onload = function() {
-      createBoard()
-    }
-
-    function createBoard(){
-      for (let index = 0; index < 5; index += 1) {
-        const pixelParent = document.createElement('section');
-        pixelBoard.appendChild(pixelParent);
-        for (let index2 = 0; index2 < 5; index2 += 1) {
-        let pixel = document.createElement('div');
-        pixel.classList = 'pixel';
-        pixelParent.appendChild(pixel);
-        };
-      };
+window.onload = function() {
+  createBoard();
+}
+function createBoard(){
+  for (let index = 0; index < 5; index += 1) {
+    const pixelParent = document.createElement('section');
+    pixelBoard.appendChild(pixelParent);
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelParent.appendChild(pixel);
     };
   };
 };
@@ -47,7 +36,7 @@ pixelBoard.addEventListener('click', function(event) {
   };
 });
 
-button.addEventListener('click', function(event) {
+button.addEventListener('click', function() {
   pixelBoard.innerHTML = null;
-  createBoard()
+  createBoard();
 });
