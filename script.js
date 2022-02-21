a30434b5e4c5b2b00754fa38447eadcf57c6d861
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-21 06:10:14 -0300

    conclui rec ate 9.

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 // 4 - Cria quadro de pixel, pixels dentro do quadro, classes e id.
 //posso tentar usar uma function que será usada dentro de outra function para definir
 //quantos quadrados serão criados(quantos divs serão criados com createElement)
-function createBoard(){
-let n = 5
-  for (let index = 0; index < n; index += 1) {
-    let pixelBoard = document.getElementById('pixel-board');
-    let pixelParent = document.createElement('section');
-    pixelBoard.appendChild(pixelParent);
-    for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-  
-    let pixel = document.createElement('div');
-    pixel.classList = 'pixel';
-    pixelBoard.appendChild(pixel);
-    };
-  }
-};
-createBoard();
-// //12
-// function createRandomColor() {
-//   let first3Numbers = Math.floor(Math.random() * 255);
-//   let second3Numbers = Math.floor(Math.random() * 255);
-//   let third3Numbers = Math.floor(Math.random() * 255);
-//   let randomNumbers = `rgb(${first3Numbers}, ${second3Numbers}, ${third3Numbers})`;
-//   return randomNumbers;
-// }
+let color = " ";
+const button = document.getElementById('clear-button');
+const palette = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board');
 
-// function getColor() {
-//   let color1 = document.querySelector('#color1');
-//   let color2 = document.querySelector('#color2');
-//   let color3 = document.querySelector('#color3');
-//   color1.getElementsByClassName.backgroundColor = createRandomColor;
-//   color2.getElementsByClassName.backgroundColor = createRandomColor;
-//   color3.getElementsByClassName.backgroundColor = createRandomColor;
-// }
+for (let index = 0; index < 5; index += 1) {
+  const pixelParent = document.createElement('section');
+  pixelBoard.appendChild(pixelParent);
+  for (let index2 = 0; index2 < 5; index2 += 1) {
+  
+    window.onload = function() {
+      createBoard()
+    }
+
+    function createBoard(){
+      for (let index = 0; index < 5; index += 1) {
+        const pixelParent = document.createElement('section');
+        pixelBoard.appendChild(pixelParent);
+        for (let index2 = 0; index2 < 5; index2 += 1) {
+        let pixel = document.createElement('div');
+        pixel.classList = 'pixel';
+        pixelParent.appendChild(pixel);
+        };
+      };
+    };
+  };
+};
+
+palette.addEventListener('click', function(event) {
+  const element = event.target;
+  if ( element === palette) {
+    return ;
+  }
+  const previosSelected = document.querySelector('.selected');
+  previosSelected.classList.remove('selected');
+  element.classList.add('selected');
+  cor = getComputedStyle(element).backgroundColor
+});
+
+pixelBoard.addEventListener('click', function(event) {
+  const element = event.target;
+  if ( element.classList.contains('pixel')) {
+    element.style.backgroundColor = cor;
+  };
+});
+
+button.addEventListener('click', function(event) {
+  pixelBoard.innerHTML = null;
+  createBoard()
+});
